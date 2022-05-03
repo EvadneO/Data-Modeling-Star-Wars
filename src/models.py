a7b86f7a91425b0384d0f8f1df28e5431911cc15
EvadneO <evadne28@gmail.com>
2022-05-03 00:46:05 +0000

    Tarea de data modeling

--- src/models.py
+++ src/models.py
@@ -8,26 +8,73 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class User(Base):
+    __tablename__= 'usuario'
+    id= Column(Integer, primary_key=True)
+    password = Column(String(250))
+    email = Column(String(250))
 
-    def to_dict(self):
-        return {}
+class Favoritos_personaje(Base):
+    __tablename__= 'favoritos_personaje'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('usuario.id'))
+    personaje_id= Column(Integer, ForeignKey('personaje.id'))
+
+class Favoritos_planetas(Base):
+    __tablename__= 'favoritos_planetas'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('usuario.id'))
+    planeta_id= Column(Integer, ForeignKey('planetas.id'))
+
+class Favoritos_starships(Base):
+    __tablename__= 'favoritos_starships'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('usuario.id'))
+    starships_id= Column(Integer, ForeignKey('starships.id'))
+
+class Personaje(Base):
+    __tablename__= 'personaje'
+    id = Column(Integer, primary_key=True)
+    height = Column(String(250))
+    mass = Column(String(250))
+    hair_color = Column(String(250))
+    eye = Column(String(250))
+    birth_year = Column(String(250))
+    gender = Column(String(250))
+
+class Planetas(Base):
+    __tablename__= 'planetas'
+    id = Column(Integer, primary_key=True)
+    model = Column(String(250))
+    starship = Column(String(250))
+    cost_in_credits = Column(String(250))
+    length = Column(String(250))
+    crew = Column(String(250))
+    passengers = Column(String(250)) 
+    max_atmosphering_speed = Column(String(250))
+    hyperdrive_rating = Column(String(250))
+    mGLT = Column(String(250))
+    cargo_capacity = Column(String(250))
+    consumables = Column(String(250))
+
+class Starship(Base):
+    __tablename__= 'starships'
+    id = Column(Integer, primary_key=True)
+    model = Column(String(250))
+    starship_class = Column(String(250))
+    cost_in_credits = Column(String(250))
+    length = Column(String(250))
+    crew = Column(String(250))
+    passengers = Column(String(250))
+    max_atmosphering_speed = Column(String(250))
+    hyperdrive_rating = Column(String(250))
+    mGLT = Column(String(250))
+    cargo_capacity = Column(String(250))
+    consumables = Column(String(250))
+
+ 
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')

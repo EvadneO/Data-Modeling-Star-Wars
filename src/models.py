8b621d6f3c1fe91a49f6641fce517d1d28a4db26
EvadneO <evadne28@gmail.com>
2022-06-20 01:47:08 +0000

    Base de dato de instagram

--- src/models.py
+++ src/models.py
@@ -9,71 +9,44 @@ from eralchemy import render_er
 Base = declarative_base()
 
 
+
 class User(Base):
-    __tablename__= 'usuario'
-    id= Column(Integer, primary_key=True)
-    password = Column(String(250))
-    email = Column(String(250))
-
-class Favoritos_personaje(Base):
-    __tablename__= 'favoritos_personaje'
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('usuario.id'))
-    personaje_id= Column(Integer, ForeignKey('personaje.id'))
+    name = Column(String(120), unique=True, nullable=False)
+    last_name = Column(String(120), unique=True, nullable=False)
+    email = Column(String(120), unique=True, nullable=False)
+    password = Column(String(120), unique=False, nullable=False)
+    fecha_nacimiento = Column(String(120), unique=False, nullable=False)
+    imagen_perfil = Column(String(120), unique=False, nullable=False)
 
-class Favoritos_planetas(Base):
-    __tablename__= 'favoritos_planetas'
+class Posts(Base):
+    __tablename__ = 'posts'
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('usuario.id'))
-    planeta_id= Column(Integer, ForeignKey('planetas.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    post_type = Column(String(120))
+    post_place = Column(String(120))
+    post_comment = Column(String(120))
+    created_at = Column(String(120))
+    updated_at = Column(String(120))
 
-class Favoritos_starships(Base):
-    __tablename__= 'favoritos_starships'
+class Posts_comentario(Base):
+    __tablename__ = 'posts_comentario'
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('usuario.id'))
-    starships_id= Column(Integer, ForeignKey('starships.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('posts.id'))
+    comment = Column(String(120))
+    created_at = Column(String(120))
+    delete_at = Column(String(120))
 
-class Personaje(Base):
-    __tablename__= 'personaje'
+class Post_likes(Base):
+    __tablename__ = 'post_likes'
     id = Column(Integer, primary_key=True)
-    height = Column(String(250))
-    mass = Column(String(250))
-    hair_color = Column(String(250))
-    eye = Column(String(250))
-    birth_year = Column(String(250))
-    gender = Column(String(250))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('posts.id'))
+    created_at = Column(String(120))
+    delete_at = Column(String(120))
 
-class Planetas(Base):
-    __tablename__= 'planetas'
-    id = Column(Integer, primary_key=True)
-    model = Column(String(250))
-    starship = Column(String(250))
-    cost_in_credits = Column(String(250))
-    length = Column(String(250))
-    crew = Column(String(250))
-    passengers = Column(String(250)) 
-    max_atmosphering_speed = Column(String(250))
-    hyperdrive_rating = Column(String(250))
-    mGLT = Column(String(250))
-    cargo_capacity = Column(String(250))
-    consumables = Column(String(250))
-
-class Starship(Base):
-    __tablename__= 'starships'
-    id = Column(Integer, primary_key=True)
-    model = Column(String(250))
-    starship_class = Column(String(250))
-    cost_in_credits = Column(String(250))
-    length = Column(String(250))
-    crew = Column(String(250))
-    passengers = Column(String(250))
-    max_atmosphering_speed = Column(String(250))
-    hyperdrive_rating = Column(String(250))
-    mGLT = Column(String(250))
-    cargo_capacity = Column(String(250))
-    consumables = Column(String(250))
-
- 
 
 
 ## Draw from SQLAlchemy base
